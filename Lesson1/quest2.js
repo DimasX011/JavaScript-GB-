7f1495d4487c545422497a75c10780b8e1140497
DimasX011 <Dimon1998daf@mail.ru>
2021-12-16 17:52:10 +0600

    Lesson1

---
+++ Lesson1/quest2.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let admin;
+let name = "Василий";
+
+admin = name;
+
+
+
+let par = 1000;
+let stra = "108";
+
+let uor = par + stra;
+console.log(uor);
+
+
+alert(admin);
+
+
+alert("число 1000 + строка 108 равно строке" + uor);
+
+
+
